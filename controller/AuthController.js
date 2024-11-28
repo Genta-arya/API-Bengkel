d99d5f0bfdbb071468b09f93b3e90df88937a5a9
M.Gentha arya pratama <74033154+Genta-arya@users.noreply.github.com>
2024-11-28 10:36:13 +0700

    Update AuthController.js

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const prisma = new PrismaClient();
 
-const secretKey = "bengkelku";
+const secretKey = "bengkelkyuu";
 
 export const generateJWTToken = (user) => {
   const payload = {
